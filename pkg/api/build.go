e25d69f03def1780ad348762bf48afcdc019012d
tunahanertekin <tunahan.ertekin13@gmail.com>
2023-02-08 12:30:41 +0300

    refactor(build): add component interface & abstracts

---
+++ pkg/api/build.go
@@ -0,0 +1,31 @@
+package api
+
+import "fmt"
+
+type BuildComponentInterface interface {
+	Build()
+	Push()
+	View()
+}
+
+type BuildComponent struct {
+	BuildComponentInterface
+	Image         string
+	BaseImage     string
+	Directory     string
+	Platforms     []string
+	Rebuild       bool
+	PushComponent bool
+}
+
+func (bc *BuildComponent) Build() {
+	fmt.Println(bc.Image, "Component is built.")
+}
+
+func (bc *BuildComponent) Push() {
+	fmt.Println(bc.Image, "Component is pushed.")
+}
+
+func (bc *BuildComponent) View() {
+	fmt.Println(bc.Image, "Component is viewed.")
+}
