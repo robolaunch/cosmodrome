dfec9e61a4e62ff6ed0e31648f626a8f6c392d6f
tunahanertekin <tunahan.ertekin13@gmail.com>
2023-02-08 15:40:16 +0300

    refactor(api): update pipeline api

--- pkg/api/pipeline.go
+++ pkg/api/pipeline.go
@@ -1,21 +1,79 @@
 package api
 
-import "github.com/dominikbraun/graph"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/dominikbraun/graph"
+	"gopkg.in/yaml.v2"
+)
 
 type PipelineInterface interface {
 	Build()
 	Push()
 	View()
+	Export()
 }
 
 type PipelineAbstract struct {
-	PipelineInterface
-	Registry       string
-	Name           string
-	PushComponents bool
-	DAG            graph.Graph[string, BuildComponent]
+	PipelineInterface `yaml:",omitempty"`
+	Name              string                              `yaml:"name"`
+	Registry          string                              `yaml:"registry"`
+	PushComponents    bool                                `yaml:"pushComponents"`
+	DAG               graph.Graph[string, BuildComponent] `yaml:",omitempty"`
 }
 
 type Pipeline struct {
-	PipelineAbstract
+	PipelineAbstract `yaml:"pipeline,omitempty"`
+}
+
+func NewPipeline(name string, registry string, pushComponents bool) *Pipeline {
+	pipeline := Pipeline{}
+
+	pipeline.Name = name
+	pipeline.Registry = registry
+	pipeline.PushComponents = pushComponents
+
+	return &pipeline
+}
+
+func (p *Pipeline) Build() {
+	fmt.Println(p.Name, "Pipeline is built.")
+}
+
+func (p *Pipeline) Push() {
+	fmt.Println(p.Name, "Pipeline is pushed.")
+}
+
+func (p *Pipeline) View() error {
+	pipelineYAML, err := yaml.Marshal(&p)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("\n" + string(pipelineYAML))
+	return nil
+}
+
+func (p *Pipeline) Export() error {
+
+	pipelineYAML, err := yaml.Marshal(&p)
+	if err != nil {
+		return err
+	}
+
+	directory := "pipelines"
+	_ = os.Mkdir(directory, os.ModePerm)
+
+	fileName := filepath.Join(directory, p.Name+".yaml")
+	err = ioutil.WriteFile(fileName, pipelineYAML, 0644)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Pipeline is exported to", fileName+".")
+
+	return nil
 }
